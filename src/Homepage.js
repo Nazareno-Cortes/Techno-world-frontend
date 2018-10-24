5f9583115e32afc839789a03fd68cc7fda315cde
Nazareno Cortes <naza5cortes@gmail.com>
2018-10-23 22:07:53 -0300

    modify html, css and create the products folder

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,37 +1,46 @@
 import React, {Component} from 'react';
 import './Reset.css';
 import './Homepage.css';
+import Product from './products/Product'
 
 class Homepage extends Component {
 render() {
-    return (
-        <div class="app">
-              <div class="wrapper">
-                  <div class="header">
-                    <h1>Techno World</h1>
-                  </div>
-                  <div class="menu">
-                     <ul>
-                       <li><span1>Home</span1></li>
-                       <li><span2>Products</span2></li>
-                       <li><span3>Contact us</span3></li>
-                       <li>Login</li>
-                     </ul>
-                  </div>
-                <div class="contact">
-                    <p>Attention schedule:
-                       Mon to Vier from 10 a.m. to 6 p.m. / Sat from 10 a.m. to 1 p.m.<br></br>
+return (
+  <div class="app">
+        <div class="wrapper">
+            <div class="header">
+              <h1>Techno World</h1>
+            </div>
+            <div class="menu">
+               <ul>
+                 <li><span1>Home</span1></li>
+                 <li><span2>Products</span2></li>
+                 <li><span3>Contact us</span3></li>
+                 <li><span4>Login</span4></li>
+               </ul>
+            </div>
+            <div class="content">
+            <Product show={true}/>
+            <button onClick={this.chageTitle}>Add to cart </button>
+            </div>
 
-                      <br></br>Telephone: 04545434 <br></br>
+            <div class="contact">
+            <h2>Contacts</h2>
+            </div>
+             <div class="data">
+              <p>Attention schedule:
+                 Monday to Friday from 10 a.m to 6 p.m / Saturday from 10 a.m to 1 p.m <br></br>
 
-                      <br></br> Whatsapp: +5491554788 <br></br>
+                <br></br>Telephone: 04545434 <br></br>
 
-                      <br></br>Location: Rosario, Santafe
-                      </p>
-                </div>
-              </div>
+                <br></br>Whatsapp: +5491554788 <br></br>
+
+                <br></br>Location: Rosario, Santa Fé, Argentina
+              </p>
+          </div>
         </div>
-    );
+  </div>
+);
 }
 }
 
---
+++ src/products/Product.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './Product.css';
+
+class Product extends Component {
+render() {
+return (
+	<div class="description">
+      <h3>Mouse Zowie EC1-B CS:GO Large</h3>
+       <p>
+       3591.00
+       DPI 400 / 800 / 1600 / 3200 
+       USB 2.0 / 3.0 Plug & Play
+       Buttons 5 buttons
+       Cable Length 2m / 6,6 ft
+       Report Rate 125 / 500 / 1000 Hz 
+       Size Large
+       Design Ergonomic
+       </p>
+    </div>
+
+);
+}
+}
+
+export default Product;
